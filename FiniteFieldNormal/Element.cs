3eb868ccebb1576a5b7737634d0a9f51912207a7
Volodymyr Herashchenko <cholkegn@gmail.com>
2024-01-01 12:52:50 +0200

    added more operations for elements in field

--- FiniteFieldNormal/Element.cs
+++ FiniteFieldNormal/Element.cs
@@ -1,29 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FiniteFieldNormal.Field.Field
+﻿namespace FiniteFieldNormal
 {
-    public class Element()
+    public class Element
     {
         public ulong[] array = new ulong[Field.dimension / 64 + 1];
-        //public Field field = f;
+        public Field field;
 
-        public static Element operator +(Element left, Element right)
+        public Element(Field field)
         {
-            Element result = new Element();
+            this.field = field;
+        }
+        public Element(Element element, Field field)
+        {
+            Array.Copy(element.array, array, array.Length);
+            this.field = field;
+        }
+        public Element(Element element)
+        {
+            Array.Copy(element.array, array, array.Length);
+            this.field = element.field;
+        }
+        public Element(ulong[] array, Field field)
+        {
+            this.array = array;
+            this.field = field;
+        }
+
+        public static Element operator + (Element left, Element right)
+        {
+            Element result = new Element(left.field);
             for (int i = 0; i < left.array.Length; i++)
             {
                 result.array[i] = left.array[i] ^ right.array[i];
             }
             return result;
         }
+        
+        public Element Power(Element element)
+        {
+            var result = field.One();
+            for (int i = 0; i < Field.dimension - 1; i++)
+            {
+                if (element.GetBit(i) == 1)
+                {
+                    result = field.Mult(result, this);
+                }
+                result = result.CycleShiftToRight();
+            }
+            if (element.GetBit(Field.dimension - 1) == 1)
+            {
+                result = field.Mult(result, this);
+            }
+            return result;
+
+        }
         public Element CycleShiftToRight()
         {
-            Element result = new Element();
+            Element result = new Element(field);
             int l = Field.dimension % 64;
 
             var k = array[^1] >> l - 1 & 1;
@@ -36,22 +68,66 @@ namespace FiniteFieldNormal.Field.Field
             result.array[^1] = result.array[^1] - ((ulong)1 << l);
             return result;
         }
+        public Element CycleShiftToRight(int shift)
+        {
+            Element result = new Element(field);
+            for(int i = 0; i < shift; i++)
+            {
+                result = result.CycleShiftToRight();
+            }
+            return result;
+        }
+        
         public Element CycleShiftToLeft()
         {
-            Element result = new Element();
-            ulong mask = (ulong)1 << 63;
+            Element result = new Element(field);
             for (int i = 0; i < array.Length - 1; i++)
             {
                 result.array[i] = array[i] >> 1 | array[i + 1] << 63;
             }
-            int l = Field.dimension % 64;
+            int l = (Field.dimension % 64);
             var temp = array[0] & 1;
 
-            result.array[^1] = (array[^1] >> 1) + (temp << l);
+            result.array[^1] = (array[^1] >> 1) + (temp << l - 1);
             return result;
         }
 
-
+        public Element Inversed()
+        {
+            var binary = Convert.ToString(Field.dimension - 1, 2);
+            var result = new Element(this);
+            var k = 1;           
+            for(int i = 1; i < binary.Length; i++)
+            {
+                var temp = new Element(result);
+                for(int j = 0; j < k; j++)
+                {
+                    result = result.CycleShiftToRight();
+                }
+                k = 2 * k;
+                result = field.Mult(result, temp);
+                if(binary.ElementAt(i) == '1')
+                {
+                    result = field.Mult(result.CycleShiftToRight(),this);
+                    k++;
+                }
+            }
+            result = result.CycleShiftToRight();
+            return result;
+        }
+        public Element Trace()
+        {
+            int k = BitCount();
+            k &= 1;
+            if(k == 1)
+            {
+                return field.One();
+            }
+            else
+            {
+                return field.Zero();
+            }
+        }
         public int BitCount()
         {
             int count = 0;
@@ -60,9 +136,8 @@ namespace FiniteFieldNormal.Field.Field
                 count += BitCount(number);
             }
             return count;
-
         }
-        public int BitCount(ulong u)
+        public static int BitCount(ulong u)
         {
             int count = 0;
             while (u > 0)
@@ -72,6 +147,19 @@ namespace FiniteFieldNormal.Field.Field
             }
             return count;
         }
-
+        public int GetBit(int position)
+        {
+            if (position >= Field.dimension | position < 0)
+            {
+                return 0;
+            }
+            int count = position / 64;
+            int shift = position % 64;
+            return (int)((array[count] >> shift) & 1);
+        }
+        public override string ToString()
+        {
+            return Convertor.ToBinary(this);
+        }
     }
 }
