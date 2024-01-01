f1f8c333f681aa097b7f4fabb0966cb20e6f5438
Volodymyr Herashchenko <cholkegn@gmail.com>
2024-01-01 12:53:17 +0200

    some changes

--- FiniteFieldNormal/Convertor.cs
+++ FiniteFieldNormal/Convertor.cs
@@ -1,33 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FiniteFieldNormal.Field;
-
-namespace FiniteFieldNormal
+﻿namespace FiniteFieldNormal
 {
     internal class Convertor
     {
-        public static Element fromBinary(string bin) 
+        public static Element FromBinary(string bin, Field f)
         {
-            var result = new Element();
+            var result = new Element(f);
             var str = bin.PadLeft(Field.dimension, '0');
-            for(int i = 0; i < Field.dimension; i++)
+            for (int i = 0; i < Field.dimension; i++)
             {
-                if (str.ElementAt(i) == '1') {
-                    result.array[i / 64] += (ulong)1 << (i % 64); 
+                if (str.ElementAt(i) == '1')
+                {
+                    result.array[i / 64] += (ulong)1 << (i % 64);
                 }
             }
             return result;
         }
-        public static string toBinary(Element element) 
+        public static string ToBinary(Element element)
         {
             var result = "";
             for (int i = 0; i < Field.dimension; i++)
             {
                 ulong temp = element.array[i / 64] >> (i % 64);
-                if((temp & 1) == 1)
+                if ((temp & 1) == 1)
                 {
                     result += "1";
                 }
