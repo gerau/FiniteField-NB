88fb6cf8cd1f250fd1976e262b6228910aa7dedc
Volodymyr Herashchenko <cholkegn@gmail.com>
2024-01-01 12:52:33 +0200

    added more operations for Field

--- FiniteFieldNormal/Field.cs
+++ FiniteFieldNormal/Field.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FiniteFieldNormal.Field
+﻿namespace FiniteFieldNormal
 {
     public class Field
     {
         public const int dimension = 491;
-        public List<List<int>> multiplicativeMatrix;
+        public int[][] multiplicativeMatrix;
 
         private static int Modulo(int number, int modulo)
         {
@@ -26,7 +20,7 @@ namespace FiniteFieldNormal.Field
             powerOfTwos[0] = 1;
             bool[,] multMatrix = new bool[dimension, dimension];
 
-            multiplicativeMatrix = new List<List<int>>();
+            multiplicativeMatrix = new int[dimension][];
 
             var p = dimension * 2 + 1;
             for (int i = 1; i < dimension; i++)
@@ -59,18 +53,101 @@ namespace FiniteFieldNormal.Field
                     }
                 }
             }
-
+            List<int> res = new List<int>();
             for (int i = 0; i < dimension; i++)
             {
-                multiplicativeMatrix.Add(new List<int>());
+
                 for (int j = 0; j < dimension; j++)
                 {
                     if (multMatrix[i, j])
                     {
-                        multiplicativeMatrix[i].Add(j);
+                        res.Add(j);
                     }
                 }
+                multiplicativeMatrix[i] = new int[res.Count];
+                for (int j = 0; j < res.Count; j++)
+                {
+                    multiplicativeMatrix[i][j] = res[j];
+                }
+                res.Clear();
             }
+        }
+        public byte[] MultByMatrix(byte[] vector)
+        {
+            var result = new byte[dimension];
+            for (int i = 0; i < dimension; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < multiplicativeMatrix[i].Length; j++)
+                {
+                    count += vector[multiplicativeMatrix[i][j]];
+                }
+                count &= 1;
+                result[i] = (byte)count;
+            }
+            return result;
+        }
+        private static byte MultByVector(byte[] left, byte[] right)
+        {
+            byte result = 0;
+            for (int i = 0; i < dimension; i++)
+            {
+                result += (byte)(left[i] & right[i]);
+            }
+            return (byte)(result & 1);
+        }
+
+        public Element Mult(Element left, Element right)
+        {
+            ulong[] result = new ulong[left.array.Length];
+
+
+            var tempLeft = GetPositionOfOnes(left);
+            var tempRight = GetPositionOfOnes(right);
+            for (int i = 0; i < dimension; i++)
+            {
+                var temp = MultByMatrix(tempLeft[i]);
+                var count = MultByVector(temp, tempRight[i]);
+
+                result[i / 64] |= (ulong)count << (i % 64);
+            }
+            return new Element(result, left.field);
+        }
+        private static byte[][] GetPositionOfOnes(Element element)
+        {
+            byte[][] result = new byte[dimension][];
+            for (int i = 0; i < dimension; i++)
+            {
+                result[i] = new byte[dimension];
+                if (element.GetBit(i) == 1)
+                {
+                    result[0][i] = 1;
+                }
+            }
+            for (int i = 1; i < dimension; i++)
+            {
+                result[i] = new byte[dimension];
+                for (int j = 0; j < dimension - 1; j++)
+                {
+                    result[i][j] = result[i - 1][j + 1];
+                }
+                result[i][^1] = result[i - 1][0];
+            }
+            return result;
+        }
+        public Element One()
+        {
+            Element result = new Element(this);
+            for (int i = 0; i < result.array.Length - 1; i++)
+            {
+                result.array[i] = ulong.MaxValue;
+            }
+            result.array[^1] = ulong.MaxValue & (ulong.MaxValue >> (64 - dimension % 64));
+            return result;
+        }
+        public Element Zero()
+        {
+            return new Element(this);
         }
     }
 }
